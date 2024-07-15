c93f61c568d6659dda529f240cadec684a0f2632
sao-marcuss <marcus.silva@expeditors.com>
2024-07-15 15:25:23 -0300

    Create Coffee Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { UIRouterModule } from '@uirouter/angular';
+import { CoffeeService } from './coffee-service';
+import { Coffee } from './model/coffee';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,26 @@ import { UIRouterModule } from '@uirouter/angular';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private coffeeSvc: CoffeeService) { }
+  coffee : any;
+
   title = 'angular-capstone';
+
+  printResponse(){
+    this.coffeeSvc.getAll().subscribe(coffee => {
+      this.coffee = coffee;
+    }
+  )
+    console.log(this.coffee);
+  }
+
+
+  printResponse2(){
+    this.coffeeSvc.getById('2').subscribe(coffee => {
+      this.coffee = coffee;
+    }
+  )
+    console.log(this.coffee);
+  }
+
 }
