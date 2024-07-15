5dba31b6b4ec32bf11481c89fd55919e371f1e5c
João Alonso <joao.alonso@expeditors.com>
2024-07-15 13:50:48 -0300

    add: coffee model

---
+++ src/app/model/coffee.ts
@@ -0,0 +1,17 @@
+export type FormatType  ='beans' | 'ground' | 'k-pod' | undefined
+export type RoastType = 'light' |'medium'|'medium-dark'| 'dark'| undefined
+
+export interface Coffee{
+    id: number;
+    active : boolean;
+    roaster: string;
+    variety: string;
+    size : number;
+    roast : RoastType;
+    format : FormatType;
+    grind : number;
+    origin: string[];
+    singleOrigin : boolean;
+    tastingNotes : string;
+
+}
