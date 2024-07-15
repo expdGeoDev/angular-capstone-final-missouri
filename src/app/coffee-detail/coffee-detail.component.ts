5e3b40dcaabc259659e8be299415392a407d0384
João Alonso <joao.alonso@expeditors.com>
2024-07-15 14:39:30 -0300

    add: format and roast options in component. imports

--- src/app/coffee-detail/coffee-detail.component.ts
+++ src/app/coffee-detail/coffee-detail.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Coffee, FormatType, RoastType } from '../model/coffee';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-coffee-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './coffee-detail.component.html',
   styleUrl: './coffee-detail.component.css'
 })
 export class CoffeeDetailComponent {
+
+  roastOptions : RoastType[] = ['dark','light','medium','medium-dark']
+  formatOptions : FormatType[] = ['beans','ground','k-pod'] 
   
 }
