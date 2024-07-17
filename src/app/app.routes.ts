35db10e15e6a39f3a1ad1c44cd3ccd03281bd3c7
sao-caioh <caio.henrique@expeditors.com>
2024-07-17 15:31:34 -0300

    created about and add image to home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { AboutComponent } from "./about/about.component";
 import { CoffeeDetailComponent } from "./coffee-detail/coffee-detail.component";
 import { CoffeeSvcTestComponent } from "./coffee-svc-test/coffee-svc-test.component";
 
@@ -13,5 +14,12 @@ export const routerStates = [
         url: '/coffee-svc-test',
         component: CoffeeSvcTestComponent,
         label: 'Service Tests'
+    },
+    {
+        name: 'about-detail',
+        url: '/about',
+        component: AboutComponent,
+        label: 'About Detail'
     }
+
 ]
