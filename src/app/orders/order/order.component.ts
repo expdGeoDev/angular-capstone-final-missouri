d22ea36a26c57c83bc5bcf9963258a2be0cf518c
Mainor Lobo <mainor.lobo@expeditors.com>
2024-07-17 10:39:18 -0600

    Filters and Buttons Actions

--- src/app/orders/order/order.component.ts
+++ src/app/orders/order/order.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OrderService } from './order.service';
 import { Coffee } from '../../model/Coffee';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { values } from '@uirouter/angular';
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
   orders: Coffee[] = [];
-  filter: string = '';
+  filterFields = { roaster: '', size: '', roast: '', format: '' };
 
   constructor(
     private orderService: OrderService
@@ -21,13 +23,42 @@ export class OrderComponent {
   ngOnInit() {
     this.orderService.getOrders().subscribe(orders => {
       this.orders = orders;
-      console.table(orders);
     });
   }
 
   getFiltered() {
-    return this.filter === ''
-      ? this.orders
-      : this.orders.filter((order) => order.roaster === this.filter);
+    console.log(this.filterFields.roaster,
+      this.filterFields.size,
+      this.filterFields.roast,
+      this.filterFields.format
+    );
+
+    if (
+      this.filterFields.roaster === ''
+      && Number(this.filterFields.size) === 0
+      && this.filterFields.roast.toString() === ''
+      && this.filterFields.format.toString() === ''
+    ) {
+      return this.orders;
+    }
+    else {
+      return this.orders
+        .filter((order) =>
+          this.filterFields.roaster != '' ? order.roaster.toLocaleLowerCase().includes(this.filterFields.roaster.toLocaleLowerCase()) : order.roaster
+        )
+        .filter((order) =>
+          Number(this.filterFields.size) != 0 ? order.size.toString().includes(this.filterFields.size) : order.size
+        )
+        .filter((order) =>
+          this.filterFields.roast.toString() != '' ? order.roast?.toString().toLocaleLowerCase().includes(this.filterFields.roast.toString().toLocaleLowerCase()) : order.roast
+        )
+        .filter((order) =>
+          this.filterFields.format.toString() != '' ? order.format?.toString().toLocaleLowerCase().includes(this.filterFields.format.toString().toLocaleLowerCase()) : order.format
+        );
+    }
+  }
+
+  tableAction(action: string) {
+    console.log("Table Action: " + action)
   }
 }
