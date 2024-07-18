b43f6281f88f685aaeec4fea8398bd3610fc4ee8
João Alonso <joao.alonso@expeditors.com>
2024-07-18 13:16:38 -0300

    add: shallow tests

--- src/app/coffee-detail/coffee-detail.component.shallow.spec.ts
+++ src/app/coffee-detail/coffee-detail.component.shallow.spec.ts
@@ -1,32 +1,72 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture,  TestBed } from '@angular/core/testing';
 
 import { CoffeeDetailComponent } from './coffee-detail.component';
 import { CoffeeService } from '../coffee-service';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-
+import { ChangeDetectionStrategy, ChangeDetectorRef, NO_ERRORS_SCHEMA } from '@angular/core';
+import { Transition } from '@uirouter/angular';
+import { Coffee } from '../model/coffee';
+import { By } from '@angular/platform-browser';
 describe('CoffeeDetailComponent - Shallow Tests', () => {
-  let component: CoffeeDetailComponent;
-  let mockAlertSvc;
-  let mockCoffeeSvc;
+  let fixture: ComponentFixture<CoffeeDetailComponent>;
+  let mockCoffeeSvc: jasmine.SpyObj<CoffeeService>;
+  let mockTransition : jasmine.SpyObj<Transition>;
 
+  let coffee: Coffee = {
+    "id": 1, "active": true,
+    "roaster": 'bye', "variety": 'Arabica',
+    "size": 24, "roast": 'Light',
+    "format": 'Beans', "grind": 1,
+    "origin": [''], "singleOrigin": false,
+    "tastingNotes": ""
+  }
   beforeEach(async () => {
-    mockCoffeeSvc = jasmine.createSpyObj(['getAll','getById','addCoffee']);
-    mockAlertSvc = jasmine.createSpyObj([''])
+
+    mockCoffeeSvc = jasmine.createSpyObj(['getAll','getById','addCoffee','updateCoffee']);
+    mockTransition = jasmine.createSpyObj(['params'])
+
     await TestBed.configureTestingModule({
       imports: [CoffeeDetailComponent],
-      providers:[{
-        provide : CoffeeService, useValue: mockCoffeeSvc}
+        providers:[{
+        provide : CoffeeService, useValue: mockCoffeeSvc},
+        {provide : Transition, useValue :mockTransition}
       ],
       schemas:[NO_ERRORS_SCHEMA]
     })
     .compileComponents();
     
-  
-    component = new CoffeeDetailComponent(mockCoffeeSvc, mockAlertSvc);
+    mockTransition.params.and.returnValue([{'data': null}])
+    fixture = TestBed.createComponent(CoffeeDetailComponent);
 
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Validate roaster label content', () =>{
+    fixture.detectChanges();
+    let roasterEl = fixture.debugElement.query(By.css('#lblRoaster')).nativeElement;
+    expect(roasterEl.textContent).toContain('Roaster*')
+
+  });
+
+  it('Validate roaster input content', async () =>{
+
+    // fixture.detectChanges();
+    // let roasterEl = fixture.debugElement.query(By.css('#roaster')).nativeElement;
+    // console.log('Roaster El' , roasterEl)
+    // expect(roasterEl.value).toContain('bye')
+    expect(fixture.componentInstance.coffee.roaster).toEqual('Initial Value') 
+    fixture.componentInstance.coffee = {
+        "id": 1, "active": true,
+        "roaster": 'bye', "variety": 'Arabica',
+        "size": 24, "roast": 'Light',
+        "format": 'Beans', "grind": 1,
+        "origin": [''], "singleOrigin": false,
+        "tastingNotes": ""
+      };
+      expect(fixture.componentInstance.coffee.roaster).toEqual('bye') 
+      fixture.detectChanges();
+      await fixture.whenStable();
+      let roasterEl = fixture.debugElement.query(By.css('#roaster')).nativeElement;
+      console.log('Roaster El' , roasterEl)
+      console.log(roasterEl.value)
+      expect(roasterEl.value).toContain('bye')
   });
 });
--- src/app/coffee-detail/coffee-detail.component.spec.ts
+++ src/app/coffee-detail/coffee-detail.component.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-
+import { Transition } from '@uirouter/angular';
 import { CoffeeDetailComponent } from './coffee-detail.component';
 import { CoffeeService } from '../coffee-service';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
@@ -11,21 +11,24 @@ describe('CoffeeDetailComponent', () => {
   let component: CoffeeDetailComponent;
   let mockAlertSvc : jasmine.SpyObj<AlertMessageService>;
   let mockCoffeeSvc: jasmine.SpyObj<CoffeeService>;
+  let mockTransition : jasmine.SpyObj<Transition>;
 
   beforeEach(async () => {
-    mockCoffeeSvc = jasmine.createSpyObj(['getAll','getById','addCoffee','addCoffee']);
+    mockCoffeeSvc = jasmine.createSpyObj(['getAll','getById','addCoffee','updateCoffee']);
     mockAlertSvc = jasmine.createSpyObj(['success'])
+    mockTransition = jasmine.createSpyObj(['params'])
     await TestBed.configureTestingModule({
       imports: [CoffeeDetailComponent],
       providers:[{
-        provide : CoffeeService, useValue: mockCoffeeSvc}
+        provide : CoffeeService, useValue: mockCoffeeSvc},
+        {provide : Transition, useValue :mockTransition}
       ],
       schemas:[NO_ERRORS_SCHEMA]
     })
     .compileComponents();
     
-  
-    component = new CoffeeDetailComponent(mockCoffeeSvc, mockAlertSvc);
+    mockTransition.params.and.returnValue([{'data': null}])
+    component = new CoffeeDetailComponent(mockCoffeeSvc, mockAlertSvc, mockTransition);
 
   });
 
@@ -33,9 +36,9 @@ describe('CoffeeDetailComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('saveCoffee should call services methods success', () =>{
+  it('saveCoffee should call addCoffee success', () =>{
       let coffee: Coffee = {
-        "id": 2, "active": true,
+        "id": 0, "active": true,
         "roaster": 'bye', "variety": 'Arabica',
         "size": 24, "roast": 'Light',
         "format": 'Beans', "grind": 1,
@@ -48,4 +51,20 @@ describe('CoffeeDetailComponent', () => {
     expect(mockAlertSvc.success).toHaveBeenCalled()
 
   });
+
+  it('saveCoffee should call updateCoffee success', () =>{
+        let coffee: Coffee = {
+          "id": 1, "active": true,
+          "roaster": 'bye', "variety": 'Arabica',
+          "size": 24, "roast": 'Light',
+          "format": 'Beans', "grind": 1,
+          "origin": [''], "singleOrigin": false,
+          "tastingNotes": ""
+      }
+      mockCoffeeSvc.updateCoffee.and.returnValue()
+      component.saveCoffee(coffee)
+      expect(mockCoffeeSvc.updateCoffee).toHaveBeenCalled()
+      expect(mockAlertSvc.success).toHaveBeenCalled()
+
+    });
 });
