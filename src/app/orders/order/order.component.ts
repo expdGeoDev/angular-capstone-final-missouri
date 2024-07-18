aa4a3a5a081007307c4866d2fc4f948a40674e12
sao-marcuss <marcus.silva@expeditors.com>
2024-07-18 09:51:17 -0300

    remove unused imports

--- src/app/orders/order/order.component.ts
+++ src/app/orders/order/order.component.ts
@@ -3,7 +3,6 @@ import { StateService } from '@uirouter/angular';
 import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UIRouterModule } from '@uirouter/angular';
-import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { CoffeeService } from '../../coffee-service';
 import { Coffee } from '../../model/coffee';
